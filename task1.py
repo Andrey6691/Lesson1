0c32d56c989a78a28811a23c724d89dac03c3af3
Andrey6691 <abw66@yandex.ru>
2024-09-01 19:45:15 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,18 @@
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3d program
+print(2*2+2)
+print((2+2)*2)
+a=(2*2+2)
+b=((2+2)*2)
+print(a==b)
+#4th program
+a='123.456'
+b=float(a)
+c=int(b*10)
+print(c%10)
+
+
+
